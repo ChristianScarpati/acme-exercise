1e828825dceb09df15a9d0d5f3b120d0d7cae630
ChristianScarpati <christian-mallmann@hotmail.com>
2023-03-06 14:41:47 -0300

    refactor: advance refactor controller/

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,79 +1,97 @@
-const { regExpTimes: getTimes, regExpNames: getNames } = require('../utils/global.js')
+const { regExpTimes: getTimes, regExpNames: getNames } = require("../utils/global.js");
 
 //convert the data to data Array
-const returnDataOnArray = (data) => {
-     let arrayData = []
-     let array = data.toString().split('\n')
-     for (i in array) {
-          const names = array[i].match(getNames)[0]
-          try {
-               const timeWorked = array[i].match(getTimes)[0]
-               const arrayOfTime = timeWorked.split(',')
-               arrayData.push({ [names]: arrayOfTime })
-          } catch (error) {
-               console.error('An error has occured with the data names.')
-          }
-     }
-     return arrayData
-}
+const parseDocumentData = (data) => {
+  const lines = data.toString().split("\n");
+  let arrayData = [];
+
+  for (const line of lines) {
+    const [names] = line.match(getNames) || [];
+    const [timeWorked] = line.match(getTimes) || [];
+
+    try {
+      const arrayOfTime = timeWorked.split(",");
+      arrayData.push({ [names]: arrayOfTime });
+    } catch (error) {
+      console.error(`An error has occured with the data names: ${error}`);
+    }
+  }
+
+  return arrayData;
+};
 
 //check the coincidence between employees
 const checkCoincidences = (array1, array2) => {
-     const mapObject = {};
-     let coincidences;
+  let coincidences = 0;
 
-     array1.forEach(i => mapObject[i] = false);
-     array2.forEach(i => mapObject[i] === false && (mapObject[i] = true));
+  for (const element of array1) {
+    if (array2.includes(element)) {
+      coincidences++;
+    }
+  }
 
-     coincidences = Object.keys(mapObject)
-          .map(k => ({ name: k, matched: mapObject[k] }))
-          .filter(x => x.matched == true);
+  return coincidences;
+};
 
-     return coincidences.length
-}
+const getFirstObjectKey = (object) => {
+  return Object.keys(object)[0];
+};
 
-// Nested Loop of employees.
 const IterationOfNames = (employeesArray) => {
+  const employeeSchedules = employeesArray[0][getFirstObjectKey(employeesArray[0])];
 
-     const employeeSchedules = employeesArray[0][Object.keys(employeesArray[0])]
+  employeeSchedules.forEach((_, j) => {
+    const {
+      [j]: {
+        [getFirstObjectKey(employeesArray[j])]: employee1,
+        [getFirstObjectKey(employeesArray[j])]: schedule1,
+      },
+    } = employeesArray;
 
-     for (let j = 0; j < employeeSchedules.length; j++) { 
-          for (let i = 0; i < employeeSchedules.length; i++) { 
-               
-               const employee1 = Object.keys(employeesArray[j])[0]
-               const employee2 = Object.keys(employeesArray[i])[0]
+    employeeSchedules.forEach((_, i) => {
+      const {
+        [i]: {
+          [getFirstObjectKey(employeesArray[i])]: employee2,
+          [getFirstObjectKey(employeesArray[i])]: schedule2,
+        },
+      } = employeesArray;
 
-               const schedule1 = employeesArray[j][Object.keys(employeesArray[j])]
-               const schedule2 = employeesArray[i][Object.keys(employeesArray[i])]
+      // schedule 1 and schedule 2 are the schedules of the employees
+      // employee 1 y employee 2 are the names of the employees
 
-               const coincidences = checkCoincidences(schedule1, schedule2)
+      const coincidences = checkCoincidences(schedule1, schedule2);
 
-               if (employee1 !== employee2) {
-                    console.table(`${employee1} - ${employee2} ----> coincidencias ${coincidences}`)
-               }
-          }
-     }
+      const employeesNamesJ = getFirstObjectKey(employeesArray[j]);
+      const employeeNamesI = getFirstObjectKey(employeesArray[i]);
 
-}
-
+      if (employee1 !== employee2) {
+        console.table(
+          `${employeesNamesJ} - ${employeeNamesI} ----> coincidencias ${coincidences}`
+        );
+      }
+    });
+  });
+};
 
 const typesOfData = (data) => {
-     if (typeof data == 'string') return false
-     if (typeof data == 'object') return true
+  if (typeof data == "string") return false;
+  if (typeof data == "object") return true;
 
-     return false
-}
+  return false;
+};
 
 const filenameTxt = (arg) => {
-     if (arg === './documents/employees.txt') {
-          return ('./documents/employees.txt')
-     } else {
-          return arg
-     }
-}
+  if (arg === "./documents/employees.txt") {
+    return "./documents/employees.txt";
+  } else {
+    return arg;
+  }
+};
 
-
-
-
-module.exports = { checkCoincidences, IterationOfNames, returnDataOnArray, typesOfData, filenameTxt }
-
+module.exports = {
+  checkCoincidences,
+  IterationOfNames,
+  parseDocumentData,
+  typesOfData,
+  filenameTxt,
+};
--- index.js
+++ index.js
@@ -1,53 +1,45 @@
+const readline = require("readline");
+const fs = require("fs");
+const controllers = require("./controller/employeeController");
 
-
-const readline = require('readline')
-const fs = require('fs')
-const controllers = require('./controller/employeeController')
-
-const returnDataOnArray = controllers.returnDataOnArray
-const checkCoincidences = controllers.checkCoincidences
-const IterationOfNames = controllers.IterationOfNames
+const parseDocumentData = controllers.parseDocumentData;
+const IterationOfNames = controllers.IterationOfNames;
 
 // Creates the interface for reading data
 var readInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
-
-
 
 // reads the path to the file to work with(default: test/employees.txt)
 readInterface.question("Welcome to ACME's App => ", (path) => {
-    let filename = path == '' ? './documents/employees.txt' : path
-    try {
-        //read the  ~/documents/employees.txt file
-        fs.readFile(filename, 'utf8', function (error, data) {
-            if (error) {
-                console.log('There is an Error', error.message)
-                readInterface.close()
-                throw error
-            }
+  let filename = path == "" ? "./documents/employees.txt" : path;
+  try {
+    //read the  ~/documents/employees.txt file
+    fs.readFile(filename, "utf8", function (error, data) {
+      if (error) {
+        console.log("There is an Error", error.message);
+        readInterface.close();
+        throw error;
+      }
 
-            const employeesArray = returnDataOnArray(data)
-            //run the function to determine the match for each employee
-            IterationOfNames(employeesArray)
-            readInterface.close()
-        })
+      const employeesArray = parseDocumentData(data);
+      //run the function to determine the match for each employee
+      IterationOfNames(employeesArray);
+      readInterface.close();
+    });
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.log("File not found");
+    } else {
+      throw error;
     }
-    catch (error) {
-        if (error.code === 'ENOENT') {
-            console.log('File not found')
-        } else {
-            throw error
-        }
-    }
-})
-
+  }
+});
 
 //  Displays a Dialog and exit the process
 
-readInterface.on('close', function () {
-    console.log("\n\n Interface closed")
-    process.exit(0)
-})
-
+readInterface.on("close", function () {
+  console.log("\n\n Interface closed");
+  process.exit(0);
+});
